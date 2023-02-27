073b3b50b4896958324ea7377d195b0d6bf6199e
MateusHoffman <mateushoffmandev@gmail.com>
2023-02-27 13:15:49 -0300

    Export screens

--- src/screens/index.tsx
+++ src/screens/index.tsx
@@ -1,7 +1,11 @@
 import Home from './Home'
 import DetailsCEP from './DetailsCEP'
+import FavoriteCEP from './FavoriteCEP'
+import Welcome from './Welcome'
 
 export {
   Home,
-  DetailsCEP
+  DetailsCEP,
+  FavoriteCEP,
+  Welcome,
 }
