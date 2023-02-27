1505fc1199f7cc9c2a7106fc8514e1a78861f463
MateusHoffman <mateushoffmandev@gmail.com>
2023-02-27 13:20:18 -0300

    Add new routes: Welcome and FavoriteCEP

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import { Home, DetailsCEP } from "../../src/screens"
+import { Home, DetailsCEP, FavoriteCEP, Welcome } from "../../src/screens"
 
 const { Navigator, Screen } = createNativeStackNavigator();
 
@@ -10,9 +10,11 @@ export default function AppRoute() {
   return (
     <>
       <NavigationContainer>
-        <Navigator screenOptions={{ headerShown: false}} initialRouteName="Home">
+        <Navigator screenOptions={{ headerShown: false}} initialRouteName="Welcome">
+          <Screen name="Welcome" component={Welcome} />
           <Screen name="Home" component={Home} />
           <Screen name="DetailsCEP" component={DetailsCEP} />
+          <Screen name="FavoriteCEP" component={FavoriteCEP} />
         </Navigator>
       </NavigationContainer>
     </>
